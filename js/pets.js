d15238930490f573eaced535e7587b9bcac48922
indroji-mondal <indro.cse.bu@gmail.com>
2024-10-06 19:51:57 +0600

    detailsmodal okay

--- js/pets.js
+++ js/pets.js
@@ -13,22 +13,27 @@ const getYear= (dob)=>{
     // typeof dob !=='string'
     let year='2024';
     if(dob){
-
        year= dob.slice(0,4);
-       
-       
     }
     else{
         year=2024;
     }
     return year;
 }
+const getPetDetailsLines= (pet_details) =>{
+   console.log(typeof pet_details);
+   const result = pet_details.split('. ');
+   console.log(result);
+   return result;
+}
+
 const displayPets = (pets) => {
     const left = document.getElementById('left');
 
     for (const pet of pets) {
         const newDiv = document.createElement('div');
         const year= getYear(pet.date_of_birth);
+        const detailsLines= getPetDetailsLines(pet.pet_details);
         newDiv.classList.add('border', 'p-3', 'border-b1','rounded-md');
         newDiv.innerHTML = `
         <img class="rounded-md" src="${pet.image}" alt="">
@@ -43,6 +48,7 @@ const displayPets = (pets) => {
         <p><i class="fa-solid fa-calendar-days "></i> Birth: ${pet.date_of_birth?year:'2024'}</p>
         <p><i class="fa-solid fa-venus"></i> Gender: ${pet.gender?pet.gender:'Male'}</p>
         <p><i class="fa-solid fa-dollar-sign"></i> Price : ${pet.price?pet.price:'1200'}$</p>
+        
         <hr>
     
         <div class="pt-5 flex gap-5 justify-center">
@@ -93,6 +99,54 @@ const displayPets = (pets) => {
         const detailsButton = newDiv.querySelector('.details');
         detailsButton.onclick = () =>{
             const detailsModal = document.getElementById('detailsModal');
+            const detailsListHTML = detailsLines.map(line => `<li>${line}.</li>`).join(''); 
+            detailsModal.innerHTML=
+            `
+            <div class="modal-box space-y-2 rounded-md">
+               
+            <img class="w-full rounded-md" src="${pet.image}" alt="">
+
+            
+              <h2 class="text-2xl font-bold ">${pet.pet_name?pet.pet_name:'Tom'}</h3>
+            <div class="space-y-2">
+              <div>
+                <div class="grid grid-cols-2 gap-5">
+                <p class="flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4"> 
+                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
+                </svg> 
+                Breed: ${pet.breed?pet.breed:'Golden Retrieve'}
+                </p> 
+                <p><i class="fa-solid fa-calendar-days "></i> Birth: ${pet.date_of_birth?year:'2024'}</p>
+                </div>
+                <div class="grid grid-cols-2 gap-5">
+                <p><i class="fa-solid fa-venus"></i> Gender:  ${pet.gender?pet.gender:'Male'}</p>
+                <p><i class="fa-solid fa-dollar-sign"></i> Price : ${pet.price?pet.price:'1200'}$</p>
+                </div> 
+                <p><i class="fa-solid fa-venus"></i> Vaccinated Status : ${pet.vaccinated_status?pet.vaccinated_status:'Partially'}</p>
+              </div> 
+               
+              <div>
+                 <h3 class="font-extrabold ">Details Information</h3>
+                 <p>Meet our lovable pet! Full of energy and affection, this furry friend brightens every day. Check out the details below to learn more about their unique traits!</p>
+                  <ul class="pl-10 list-disc">
+                     
+                  ${detailsListHTML}
+                     
+                  
+                  </ul>
+              </div>
+
+            </div>
+
+              <form class="pt-3 w-full " method="dialog">
+              
+                <button class=" w-full px-3 py-2 font-bold rounded-md bg-btn text-primary">Cancel</button>
+
+             </form>
+
+        </div>
+            `;
             detailsModal.show();
         }
 
